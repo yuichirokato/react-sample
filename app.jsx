da98583cbcdc199c812d0137bb29c41aba0bb6f0
yuichirokato <ikmy0709@gmail.com>
2016-04-23 15:18:16 +0900

    initial commit

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(porps);
+  }
+
+  render() {
+    return <h1>Hello! React</h1>;
+  }
+}
+
+React.render(
+  <Hello />,
+  document.getElementById("container");
+);
